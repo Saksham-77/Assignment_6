65b7db00439dae7896fe8a13c5bd6068871c5a8d
Saksham <118709539+Saksham-77@users.noreply.github.com>
2023-01-28 01:28:29 +0530

    Update Q8.py

--- Q8.py
+++ Q8.py
@@ -1,23 +1,24 @@
 class py_solution:
- def threeSum(self, nums):
-        nums, result, i = sorted(nums), [], 0
-        while i < len(nums) - 2:
-            j, k = i + 1, len(nums) - 1
-            while j < k:
-                if nums[i] + nums[j] + nums[k] < 0:
-                    j += 1
-                elif nums[i] + nums[j] + nums[k] > 0:
-                    k -= 1
-                else:
-                    result.append([nums[i], nums[j], nums[k]])
-                    j, k = j + 1, k - 1
-                    while j < k and nums[j] == nums[j - 1]:
-                        j += 1
-                    while j < k and nums[k] == nums[k + 1]:
-                        k -= 1
-            i += 1
-            while i < len(nums) - 2 and nums[i] == nums[i - 1]:
-                i += 1
-        return result
+ def threeSum(self, lst):
+     pos, neg, soln = [], [], []
+     lst.sort()
+     for _ in lst:
+         if _ > 0:
+             pos.append(_)
+         else:
+             neg.append(_)
+     for i in neg:
+         for j in pos:
+             for k in range(len(pos)):
+                 if i + j + pos[k] == 0:
+                     z = (i, j, pos[k])
+                     soln.append(z)
 
+     for c in pos:
+         for v in neg:
+             for b in range(len(neg)):
+                 if c + v + neg[b] == 0:
+                     x = (c, v, neg[b])
+                     soln.append(x)
+     print(soln)
 print(py_solution().threeSum([-25, -10, -7, -3, 2, 4, 8, 10]))
